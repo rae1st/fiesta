c9caf8c96f759028553eeb44b8050066eade176b
raeist <heyiamraeist@gmail.com>
2025-08-26 15:29:48 +0530

    Create __init__.py

---
+++ fiesta/models/__init__.py
@@ -0,0 +1,7 @@
+from .user import User
+from .guild import Guild
+from .channel import Channel
+from .message import Message
+from .role import Role
+
+__all__ = ["User", "Guild", "Channel", "Message", "Role"]
